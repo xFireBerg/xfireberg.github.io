9b3805936cccc6f5ade572c63c222fac30de9ad2
xFireBerg <32838402+xFireBerg@users.noreply.github.com>
2025-09-05 23:59:43 +0000

    Deploying to gh-pages from @ xFireBerg/xfireberg.github.io@a089bb5ac0ce00610c78b5922cbaa09f592b4ace 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,26 +9,12 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
+  },{id: "nav-teaching",
           title: "teaching",
           description: "Active Courses",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
-          },
-        },{id: "nav-submenus",
-          title: "submenus",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/_pages/dropdown/";
           },
         },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
